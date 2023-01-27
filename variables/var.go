46314d002567dff8d2b1d3bc0cc9876268d14f03
blaiseniyo <blaiseniyonkuru12@gmail.com>
2023-01-27 11:33:01 +0200

    add comments in var file

--- variables/var.go
+++ variables/var.go
@@ -6,19 +6,30 @@ import(
 	"strconv"
 )
 
-var (
-	name,city string
-	age,dob int
-	number = "5"
-)
+// Global declaration of variables
+
+// var (
+// 	name,city string
+// 	age,dob int
+// 	number = "5"
+// )
 
 func main(){
+
+	// Local declaration of varaible
+	name := "Peter"
+	city := "Kigali" 
+	age  := 45
+	dob  := 1878
+	number := "5"
 	fmt.Println("My name is:",name,"and I stay at ", city)
 	fmt.Println("I was born in", dob, "and so I am ",age,"old")
 
+	// Check variable Type
 	fmt.Println("Type of name variable is:",reflect.TypeOf(name))
 	fmt.Println("Type of name dob is", reflect.TypeOf(dob))
 	
+	// change string to int
 	iNumber,err := strconv.Atoi(number)
 
 	if err == nil{
