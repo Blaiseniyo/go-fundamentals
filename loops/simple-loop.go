1d6e8a8ef3aba9712ddbbe97defc1e533bacf28d
blaiseniyo <blaiseniyonkuru12@gmail.com>
2023-01-30 13:29:07 +0200

    add loop stament with break and continue statements

---
+++ loops/simple-loop.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main (){
+	
+	for timer:=10; timer >= 0; timer--{
+		if timer == 0 {
+			fmt.Println("Done!")
+			break
+		}else if timer%2 == 0{
+			continue 
+		}
+		fmt.Println(timer)
+		time.Sleep(1*time.Second)
+	}
+}
